795dd955cd46d4703147a39ab5c2d13e534cac9f
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2021-06-30 23:16:33 -0700

    Add information on contact page

--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,16 @@ import './Contact.css'
 function Contact() {
     return (
         <div className="contact">
-            <h1>Contact movix</h1>
+            <h1>Contact movix 🎉</h1>
+
+            <p>Feel free to contact me anytime regarding website feedback, feature requests or just to chat!</p>
+            
+            <h2>Links</h2>
+            <p>Github: github.com/SYM1000</p>
+            <p>LinkedIn: linkedin.com/in/santiago-yeomans</p>
+            <p>Twitter: twitter.com/SYM1001</p>
+            <p>AppStore: apps.apple.com/us/developer/santiago-yeomans/id1519365510</p>
+            <p>Portfolio: www.santiagoyeomans.com</p>
         </div>
     )
 }
