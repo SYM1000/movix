78dbd5cfaedd0bf7d57b9318f1d995d4e0f6d112
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2021-06-30 22:48:03 -0700

    Create contact page

---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Contact.css'
+
+function Contact() {
+    return (
+        <div className="contact">
+            <h1>Contact movix</h1>
+        </div>
+    )
+}
+
+export default Contact
