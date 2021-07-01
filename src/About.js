614ce29ba35025961f1231a2921b2b84926c7bbe
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2021-06-30 22:47:13 -0700

    Create About section

---
+++ src/About.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './About.css'
+
+function About() {
+    return (
+        <div className="about">
+            <h1>About movix</h1>
+
+            <h2>Introduction</h2>
+            <p>
+                I'm known among my friends as the cinephile who always recommends the best movies of all sorts from indie movies to the big blockbusters movies. Not so long ago I found myself struggling to answer my friends at 3AM in the morning when they were looking for something cool to watch that they knew for sure they would enjoy, so I decided to build movix for them. moviex uses the movies you like the most to recommend new movies you will enjoy, therefore you don't waste your time on a movie you will dislike.
+            </p>
+
+            <h2>Current State</h2>
+            <p>
+                Right now movix is in its early stages of life, but I will enhance movix as much as I can.
+            </p>
+            <p>
+                New upgrades will get released soon...
+            </p>
+
+            <h2>Author</h2>
+            <p>
+                Movix was built with ❤️ by Santiago Yeomans from 🇲🇽 .
+            </p>
+
+        </div>
+    )
+}
+
+export default About
