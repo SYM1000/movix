549e207580700b12c1c82fadc759de23a2aea1ec
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2021-07-01 00:21:14 -0700

    Add router to diferent app pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Hero from './Hero';
 import HomeSection from './HomeSection';
 import OurFavoriteMovies from './OurFavoriteMovies.json'
 import LikeableMovies from './LikeableMovies.json'
-import {useState} from 'react' 
+import {useState} from 'react'
+import About from './About';
+import Contact from './Contact';
+import Privacy from './Privacy';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
 
@@ -19,25 +23,36 @@ function App() {
   function updateRecommendedMovies() {
     setRecommendations(localStorage.getObj('recommendations'))
   }
-  
-  return (
-    // BEM naming convention
-    <div className="app">
 
-      <Header update_function={updateLikedMovies} update_recommendations={updateRecommendedMovies}/>
-
+  const Home = () => {
+    return (<div className ="home">
       <Hero />
-
-      {/* Section for Recommendations */}
       <HomeSection section_title="Your Liked Movies" section_data={likedMovies}/>
       <HomeSection section_title="Your Recommendations" section_data={ recommendations }/>
       {/* <HomeSection section_title="More Likeable Movies" section_data={LikeableMovies}/> */}
       <HomeSection section_title="Our Favorite Movies" section_data={OurFavoriteMovies}/>
-      
+    </div>);
+  }
+  
+  return (
+    // BEM naming convention
+    <Router>
+    
+    <div className="app">
+
+    <Header update_function={updateLikedMovies} update_recommendations={updateRecommendedMovies}/>
+
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" exact component={About} />
+      <Route path="/contact" exact component={Contact} />
+      <Route path="/privacy" exact component={Privacy} />
+      </Switch>
 
       <Footer />
 
     </div>
+    </Router>
   );
 }
 
