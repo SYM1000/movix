52eb3fddbb261bdf927a2616633446cd2e6fd75e
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2021-07-01 13:14:28 -0700

    Add new hero image

--- src/Hero.js
+++ src/Hero.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import "./Hero.css"
-import banner from './images/PostersBanner.png'
-import banner2 from './images/FullPostersBanner.png'
-import banner3 from './images/Posters.png'
+import banner3 from './images/hero.png'
 
 function Hero() {
     return (
